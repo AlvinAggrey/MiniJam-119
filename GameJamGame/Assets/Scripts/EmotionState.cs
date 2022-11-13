29a8e4f90adf694fa62762a14cfda9068260dc71
Alvin Aggrey <40688105+AlvinAggrey@users.noreply.github.com>
2022-11-13 00:35:38 +0000

    Added mob and emotional state Scripts

---
+++ GameJamGame/Assets/Scripts/EmotionState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//from -2 to 2
+public enum EmotionState
+{
+    ExNegative = -2,
+    Negative,
+    Neutral,
+    Positive,
+    EXPositive,
+}
